c455b61e9d88f32dfa516fdd341dc6efe5250e3a
Janq20 <Janoxgra@gmail.com>
2025-11-19 08:30:33 +0100

    init: Inicjalizacja projektu w Visual Studio 2022, dodanie .gitignore

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <vector>
+#include "Sortowanie.h" 
+
+using namespace std;
+
+int main() {
+    cout << "Projekt: Sortowanie przez Scalanie (MergeSort)" << endl;
+    return 0;
+}
