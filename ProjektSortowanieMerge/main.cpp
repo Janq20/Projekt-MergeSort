382f2c12876b754f24f71cfb4f315bf6956311b1
Janq20 <janoxgra@gmail.com>
2025-11-21 08:25:45 +0100

    feat: dodanie szablonu funkcji wypisujacej tablice

--- ProjektSortowanieMerge/main.cpp
+++ ProjektSortowanieMerge/main.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+// Funkcja pomocnicza do wyswietlania zawartosci wektora
+template <typename T>
+void wypiszTablice(const vector<T>& tablica) {
+    for (const auto& element : tablica) {
+        cout << element << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     cout << "=== PROJEKT: MERGE SORT ===" << endl;
     // TODO: Implementacja testow dla int i double
