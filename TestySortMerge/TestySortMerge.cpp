7e03c24e4e0b7529666540dc24e08c87ae7a29fb
Janq20 <janoxgra@gmail.com>
2025-11-28 08:33:42 +0100

    Test 4: Obsluga tablicy z samymi liczbami ujemnymi

--- TestySortMerge/TestySortMerge.cpp
+++ TestySortMerge/TestySortMerge.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-// 3. Sprawdzenie sortowania losowej listy
-TEST(TestySortowania, SortujeLosowaTablice) {
-    std::vector<int> tablica = { 12, 7, 14, 9, 10, 11 };
-    std::vector<int> oczekiwana = { 7, 9, 10, 11, 12, 14 };
+// 4. Poprawnie sortuje tablicę tylko z liczbami ujemnymi
+TEST(TestySortowania, SortujeTylkoUjemne) {
+    std::vector<int> tablica = { -10, -2, -30, -5 };
+    std::vector<int> oczekiwana = { -30, -10, -5, -2 };
     Sortowanie<int> sorter;
     sorter.sortuj(tablica);
     EXPECT_EQ(tablica, oczekiwana);
